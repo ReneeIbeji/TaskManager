6fc60f2e895d7c8f7824540d50eda48c28bc8f99
Renee Ibeji <reneeibeji@gmail.com>
2023-11-17 22:04:50 +0000

    Setup github pages

--- scripts/script.js
+++ scripts/script.js
@@ -93,7 +93,7 @@ window.onload = function(){
 
     
     newTaskButton.addEventListener("click", function() {
-        tasks.addTask("Test","Description");
+        tasks.addTask("Task name","Description");
         displayTasks(tasks);
     }); 
 }
